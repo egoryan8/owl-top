f9b504a9fe3cd8446354e4c0a6fd1ae36e0ece1f
Fogel Egor <fogel_92@bk.ru>
2022-09-04 22:56:13 +0700

    feat: add font, and variables of colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>OwlTop - лучший агрегатор курсов</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
