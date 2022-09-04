b6a75ffb95f5686a1711ccd0c091d8cb8a42b67d
Fogel Egor <fogel_92@bk.ru>
2022-09-04 22:25:33 +0700

    feat: add document component

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ru">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
