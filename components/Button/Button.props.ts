79fa884863ce10d38344f1781317302c7286aabc
Fogel Egor <fogel_92@bk.ru>
2022-09-07 02:22:24 +0700

    feat: add HTMLProps to Button component

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,7 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children: ReactNode;
   appearance: 'primary' | 'ghost';
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
-const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+const Button = ({ appearance, children, ...props }: ButtonProps): JSX.Element => {
   return (
     <button
       className={cn(styles.button, {
         [styles.primary]: appearance === 'primary',
         [styles.ghost]: appearance === 'ghost',
-      })}>
+      })}
+      {...props}>
       {children}
     </button>
   );
