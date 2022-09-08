55651f2f3d452739db1de1cde75288857d61e184
Fogel Egor <fogel_92@bk.ru>
2022-09-08 23:51:20 +0700

    feat: add paragraph component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
-import ArrowIcon from './arrow.svg';
 
 const Button = ({ appearance, arrow = 'none', children, ...props }: ButtonProps): JSX.Element => {
   return (
