c919796950f9e0adf4e32cd5806206a3d9fb3317
Fogel Egor <fogel_92@bk.ru>
2022-09-20 15:54:05 +0700

    feat: make footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import { FooterProps } from './Footer.props';
+import { format } from 'date-fns';
 import cn from 'classnames';
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer {...props} className={cn(className, styles.footer)}>
+      <p>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</p>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
