7c5e8da6318ecbd008b517cfcb4ca07ab1c8d29d
Fogel Egor <fogel_92@bk.ru>
2022-11-30 19:06:22 +0700

    feat: add yandex metric

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import {YMInitializer} from 'react-yandex-metrika';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  })
   return (
     <>
       <Head>
         <title>OwlTop - лучший агрегатор курсов</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
@@ -16,6 +24,7 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
         <meta property="og:locale" content="ru_RU"/>
       </Head>
+      <YMInitializer accounts={[]} options={{webvisor: true, defer: true}} version={"2"}/>
       <Component {...pageProps} />
     </>
   );
