e19b8232343233def92c7fe0b6b8a7bc244f18fc
Fogel Egor <fogel_92@bk.ru>
2022-11-30 19:14:28 +0700

    feat: husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
-import ym from 'react-yandex-metrika';
-import {YMInitializer} from 'react-yandex-metrika';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
-  router.events.on('routeChangeComplete', (url: string) => {
-    if (typeof window !== 'undefined') {
-      ym('hit', url);
-    }
-  })
   return (
     <>
       <Head>
@@ -24,7 +17,6 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
         <meta property="og:locale" content="ru_RU"/>
       </Head>
-      <YMInitializer accounts={[]} options={{webvisor: true, defer: true}} version={"2"}/>
       <Component {...pageProps} />
     </>
   );
